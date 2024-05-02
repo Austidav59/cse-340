16f5871af1db2273f6b9ac1194e216a8d75a6901
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2024-05-02 13:44:09 -0500

    changes

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -232,3 +232,22 @@
 --     5
 --   );
 
+-- DELETE FROM public.account
+-- WHERE account_firstname = 'Tony'
+
+-- UPDATE public.inventory 
+-- SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior') 
+-- WHERE inv_id = 10;
+
+-- SELECT c.classification_name, i.inv_model 
+-- FROM classification c 
+-- INNER JOIN inventory i 
+-- ON i.classification_id = c.classification_id 
+-- WHERE c.classification_id = 2;
+
+-- UPDATE public.inventory 
+--     SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/');
+
+-- UPDATE public.inventory
+--     SET inv_thumbnail = REPLACE(inv_thumbnail,  '/images/', '/images/vehicles/');
+
